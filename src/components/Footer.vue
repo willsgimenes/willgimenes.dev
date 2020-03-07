84a0ed5822e54e9b198ce9a68baed495e3a0b5d3
willsgimenes <willsgimenes@gmail.com>
2020-03-07 17:29:38 -0300

    adding text to home

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="footer">
+        <p>Brazilian Software Developer based in São Paulo (BR).Senior Front-end engineer at <a href="htttp://www.gympass.com" target="_blank">@gympass</a></p>
+        <p><a href="https://www.github.com/willsgimenes" target="_blank">Github</a> <a href="https://www.linkedin.com/in/william-gimenes/" target="_blank">Linkedin</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Footer',
+    }
+</script>
+
+<style scoped>
+    div#footer {
+        text-align: left;
+        bottom: 0;
+        font-size: 14px;
+        left: 20px;
+        height: 80px;
+        padding-top: 16px;
+        position: absolute;
+        width: 100%;
+    }
+
+    p {
+        line-height: 1;
+    }
+</style>
